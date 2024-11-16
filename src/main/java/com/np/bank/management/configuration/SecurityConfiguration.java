ad801ce424042f055a0d2fc68fb20aa1427339db
npvishwajith123 <npvishwajith@gmail.com>
2024-11-16 18:44:36 +0530

    Updated SecurityConfiguration.java

--- src/main/java/com/np/bank/management/configuration/SecurityConfiguration.java
+++ src/main/java/com/np/bank/management/configuration/SecurityConfiguration.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.web.configurers.HeadersCon
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -59,7 +60,7 @@ public class SecurityConfiguration {
     }
 
     @Bean
-    APIAuthenticationEntryPoint authenticationEntryPoint() {
+    AuthenticationEntryPoint authenticationEntryPoint() {
         return new APIAuthenticationEntryPoint();
     }
 }
